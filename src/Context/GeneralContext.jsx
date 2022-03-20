7f9facd6b85f3ccb4c04b43450fa259204519c67
Isku Esenov <snvislm2021@gmail.com>
2022-03-20 15:11:49 +0600

    asd

--- src/Context/GeneralContext.jsx
+++ src/Context/GeneralContext.jsx
@@ -1,9 +1,24 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
+import axios from "axios";
 
 export const GeneralContext = createContext([]);
 
 export const Generalprovider = ({ children }) => {
   const [allReviews, setAllReviews] = useState([]);
+  const URL = "https://randomuser.me/api/?gender=female";
+
+  const getData = async () => {
+    try {
+      const req = await axios.get(URL);
+      console.log(req);
+      setAllReviews(req.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    getData();
+  }, []);
 
   const data = {
     allReviews: allReviews,
