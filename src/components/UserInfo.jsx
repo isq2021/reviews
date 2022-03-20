c826c056ecc31f33b73d6d3064e1f13e767ae48f
Isku Esenov <snvislm2021@gmail.com>
2022-03-20 16:10:14 +0600

    asD

--- src/components/UserInfo.jsx
+++ src/components/UserInfo.jsx
@@ -1,72 +1,34 @@
-import React from "react";
 import {
-  Carousel,
-  CarouselCaption,
-  CarouselControl,
-  CarouselIndicators,
-  CarouselItem,
-} from "reactstrap";
+  Card,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  Typography,
+} from "@mui/material";
+import React from "react";
 
 function UserInfo() {
   return (
     <>
-      <Carousel
-        activeIndex={0}
-        next={function noRefCheck() {}}
-        previous={function noRefCheck() {}}>
-        <CarouselIndicators
-          activeIndex={0}
-          items={[
-            {
-              altText: "Slide 1",
-              caption: "Slide 1",
-              key: 1,
-              src: "https://picsum.photos/id/123/1200/600",
-            },
-            {
-              altText: "Slide 2",
-              caption: "Slide 2",
-              key: 2,
-              src: "https://picsum.photos/id/456/1200/600",
-            },
-            {
-              altText: "Slide 3",
-              caption: "Slide 3",
-              key: 3,
-              src: "https://picsum.photos/id/678/1200/600",
-            },
-          ]}
-          onClickHandler={function noRefCheck() {}}
-        />
-        <CarouselItem
-          onExited={function noRefCheck() {}}
-          onExiting={function noRefCheck() {}}>
-          <img alt="Slide 1" src="https://picsum.photos/id/123/1200/600" />
-          <CarouselCaption captionHeader="Slide 1" captionText="Slide 1" />
-        </CarouselItem>
-        <CarouselItem
-          onExited={function noRefCheck() {}}
-          onExiting={function noRefCheck() {}}>
-          <img alt="Slide 2" src="https://picsum.photos/id/456/1200/600" />
-          <CarouselCaption captionHeader="Slide 2" captionText="Slide 2" />
-        </CarouselItem>
-        <CarouselItem
-          onExited={function noRefCheck() {}}
-          onExiting={function noRefCheck() {}}>
-          <img alt="Slide 3" src="https://picsum.photos/id/678/1200/600" />
-          <CarouselCaption captionHeader="Slide 3" captionText="Slide 3" />
-        </CarouselItem>
-        <CarouselControl
-          direction="prev"
-          directionText="Previous"
-          onClickHandler={function noRefCheck() {}}
-        />
-        <CarouselControl
-          direction="next"
-          directionText="Next"
-          onClickHandler={function noRefCheck() {}}
-        />
-      </Carousel>
+      <Card sx={{ maxWidth: 345 }}>
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            height="140"
+            image="/static/images/cards/contemplative-reptile.jpg"
+            alt="green iguana"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              Lizard
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Lizards are a widespread group of squamate reptiles, with over
+              6,000 species, ranging across all continents except Antarctica
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
     </>
   );
 }
